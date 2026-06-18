c8fb34e1198456a6bf5eb6a7d5c223afe51ad709
mimi-creations <mchaydifiman@gmail.com>
2026-06-18 14:38:14 +0200

    Update db.php

--- db.php
+++ db.php
@@ -13,9 +13,10 @@ try {
         [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"; SET SESSION wait_timeout=600",
-        ]
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+        ]  
     );
+    $pdo -> exec("SET SESSION wait_timeout=600");
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }

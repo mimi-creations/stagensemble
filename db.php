49779e3a1c784ed25db38aead804eae44275760d
mimi-creations <mchaydifiman@gmail.com>
2026-06-18 14:32:37 +0200

    Update db.php

--- db.php
+++ db.php
@@ -13,7 +13,7 @@ try {
         [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"; SET SESSION wait_timeout=600",
         ]
     );
 } catch (PDOException $e) {

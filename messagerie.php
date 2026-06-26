f2ebb16017699c45caef489c64523b6f223d67d4
mimi-creations <mchaydifiman@gmail.com>
2026-06-26 10:46:27 +0200

    Update messagerie.php
    ecran

--- messagerie.php
+++ messagerie.php
@@ -99,6 +99,7 @@ if ($destinataire_id > 0) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messagerie Privée</title>
     <link rel="stylesheet" href="style.css">
     <style>

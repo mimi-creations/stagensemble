0b52fbb1c92718694370d55e3632be5b87ee925c
mimi-creations <mchaydifiman@gmail.com>
2026-06-26 10:49:13 +0200

    ecran

--- annuaire.php
+++ annuaire.php
@@ -26,6 +26,7 @@ if (!empty($recherche)) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Annuaire des anciens</title>
     <link rel="stylesheet" href="style.css">
 </head>

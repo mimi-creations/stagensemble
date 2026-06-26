93d2427ba06f3fe1fb48b211bac149e9764a4ef9
mimi-creations <mchaydifiman@gmail.com>
2026-06-26 10:48:26 +0200

    Update chat_prive.php

--- chat_prive.php
+++ chat_prive.php
@@ -43,6 +43,7 @@ $messages = $stmt->fetchAll();
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Discussion avec <?= htmlspecialchars($destinataire['prenom']) ?></title>
         <link rel="stylesheet" href="style.css">
     </head>
